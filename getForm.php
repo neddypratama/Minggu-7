7ed02393cc1a0e14aa95217bd50059828ac9ddd8
neddypratama <neddy_pratama@yahoo.com>
2022-03-30 12:09:38 +0700

    Minggu 7 - Neddy Pratama

---
+++ getForm.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+    <head></head>
+    <body>
+        <?php
+            if(isset($_GET["username"]) AND isset($_GET["email"])) {
+                echo "Welcome " . $_GET["username"] . "!<br>";
+                echo "Your email address is: " . $_GET["email"];
+            } else {
+                echo "Maaf, Anda harus mengakses halaman ini dari form1.html";
+            }
+        ?>
+    </body>
+</html>
---
+++ postForm.php
@@ -0,0 +1,8 @@
+<!DOCTYPE HTML>
+<html>
+    <head></head>
+    <body>
+        Welcome <?php echo $_POST["username"]; ?> <br>
+        Your email address is: <?php echo $_POST["email"]; ?>
+    </body>
+</html>
